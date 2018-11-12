de8b1f78919dfb82c990bedc60d5699d08262190
zoe606 <rive99erlangga@gmail.com>
2018-11-12 21:51:29 +0700

    basic config and set routes

---
+++ src/AppBundle/Controller/CategoryController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends Controller
+{
+    /**
+     * @Route("/categories", name="category_list")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('category/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/category/create", name="category_create")
+     */
+    public function createAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('category/create.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/category/edit/{id}", name="category_edit")
+     */
+    public function editAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('category/edit.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/category/delete/{id}", name="category_delete")
+     */
+    public function deleteAction(Request $request)
+    {
+        
+    }
+
+}
---
+++ src/AppBundle/Controller/EventController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends Controller
+{
+    /**
+     * @Route("/events", name="event_list")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('event/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/event/create", name="event_create")
+     */
+    public function createAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('event/create.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/event/edit/{id}", name="event_edit")
+     */
+    public function editAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('event/edit.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/event/delete/{id}", name="event_delete")
+     */
+    public function deleteAction(Request $request)
+    {
+        
+    }
+
+}
