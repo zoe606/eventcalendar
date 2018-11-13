d1395ff7ee394066c6c241a04abee9ae39870641
zoe606 <rive99erlangga@gmail.com>
2018-11-13 17:27:59 +0700

    Create categories

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -6,6 +6,9 @@ use AppBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoryController extends Controller
 {
@@ -29,10 +32,42 @@ class CategoryController extends Controller
      */
     public function createAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $category = new Category;
+
+        $form = $this->createFormBuilder($category)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('save', SubmitType::class, array('label' => 'Create Category', 'attr' => array('class' => 'btn btn-primary')))
+            ->getForm();
+
+            // handle request
+            $form->handleRequest($request);
+
+            // check submit
+            if ($form->isSubmitted() && $form->isValid()) {
+                $name = $form['name']->getData();
+                // get current date and time
+                $now = new \DateTime('now');
+
+                $category->setName($name);
+                $category->setCreateDate($now);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($category);
+                $em->flush();
+
+                $this->addFlash(
+                    'notice',
+                    'Category Saved'
+                );
+
+                return $this->redirectToRoute('category_list');
+            }
+
+            // render template
         return $this->render('category/create.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createView()
         ]);
+
     }
 
     /**
